3e8d0ce9c8a4a02fbc2318ac459efd4bc84ee355
v <378294113@qq.com>
2018-12-07 19:03:26 +0800

    moving board

---
+++ UnityProject/CircleWorld/Assets/Scripts/Game/GamePlay/Entity/Obstacle/ObstacleMovingBoard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace GameWish.Game
+{
+    public class ObstacleMovingBoard : ObstacleBase
+    {
+        public enum MoveDir
+        {
+            x,
+            y
+        }
+
+        [SerializeField]
+        private float m_MovingSpeed = 1;
+        [SerializeField]
+        private float m_MovingDistance = 1;
+        [SerializeField]
+        private MoveDir m_MovingDir = MoveDir.y;
+
+        private void Start()
+        {
+            if (m_MovingDir == MoveDir.x)
+            {
+                Sequence seq = DOTween.Sequence();
+                seq.Append(transform.DOLocalMoveX(m_MovingDistance, m_MovingDistance / m_MovingSpeed).SetRelative());
+                seq.Append(transform.DOLocalMoveX(-m_MovingDistance, m_MovingDistance / m_MovingSpeed).SetRelative());
+                seq.SetLoops(-1);
+            }
+            else if (m_MovingDir == MoveDir.y)
+            {
+                Sequence seq = DOTween.Sequence();
+                seq.Append(transform.DOLocalMoveY(m_MovingDistance, m_MovingDistance / m_MovingSpeed).SetRelative());
+                seq.Append(transform.DOLocalMoveY(-m_MovingDistance, m_MovingDistance / m_MovingSpeed).SetRelative());
+                seq.SetLoops(-1);
+            }
+        }
+    }
+}
